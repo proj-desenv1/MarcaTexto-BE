16ab4bd8f7a0d61e9214bf5e19c8e63441f3908b
Natan Pezzini <natanlenhard@hotmail.com>
2022-09-25 22:28:22 -0300

    chore: config database

--- src/config/connection.database.js
+++ src/config/connection.database.js
@@ -1,1 +1,20 @@
-// Configure database connection here
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const Client = require('pg').Client
+const banco =  new Client({
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    database:process.env.DB_NAME 
+})
+
+banco.connect()
+banco.query("select * from test")
+.then(results => {
+    const resultado = results.rows
+    console.table(resultado)
+})
+.finally(() => banco.end())
