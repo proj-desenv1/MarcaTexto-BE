ea9f420ae1b8fee92baf715c87663f63cd589c66
Rafael <rafafd@hotmail.com>
2022-10-06 10:12:21 -0300

    feat: Added request url on axios error handler

--- src/repository/utils/handle-axios-error.js
+++ src/repository/utils/handle-axios-error.js
@@ -1,8 +1,9 @@
-module.exports = (error) => {
+module.exports = (error, requestUrl) => {
     if(error.response) {
         const err = {
             status: error.response.status,
-            msg: error.response.data
+            msg: error.response.data,
+            url: requestUrl
         };
         throw err;
     }
