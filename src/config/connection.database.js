0a43f66b773334a339f2839b7ce86d87e2be513f
Rafael <rafafd@hotmail.com>
2022-10-06 21:27:53 -0300

    fix: Remove ssl configuration from pool

--- src/config/connection.database.js
+++ src/config/connection.database.js
@@ -1,14 +1,11 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const { Pool } = require('pg');
+const Knex = require('knex');
 
 module.exports = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  database: process.env.DB_NAME
 });
