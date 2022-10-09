6a6de23954f44892240fe192ba77cfdf74634aa1
Thiago Proença <thiago_proenca@sicredi.com.br>
2022-10-09 16:21:40 -0300

    feat: fix at updateUser controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,15 +1,22 @@
 const { response } = require("../app");
- const userBusiness = require("../business/user.business");
- const { validationResult } = require("express-validator");
+const userBusiness = require("../business/user.business");
+const userRepository = require("../repository/user.repository");
+const { validationResult } = require("express-validator");
 
- exports.updateUser = async (req, resp, next) => {
-     try {
-         validationResult(req).throw()
-         const { id } = req.params;
-         const { name, email, password } = req.body;
-         await userBusiness.updateUser(id, name, email, password);
-         resp.sendStatus(201);
-     } catch(e) {
-         next(e);
-     }
- }
+exports.updateUser = async (req, resp, next) => {
+    try {
+        validationResult(req).throw()
+        const { id } = req.params;
+        const { name, email, password } = req.body;
+        // const user = await userRepository.findUserById(id);
+
+        // if (user) {
+            await userBusiness.updateUser(id, name, email, password);
+            resp.sendStatus(200);
+        // } else {
+            // resp.sendStatus(404);
+        // }
+    } catch (e) {
+        next(e);
+    }
+}
--- src/repository/user.repository.js
+++ src/repository/user.repository.js
@@ -1,18 +1,27 @@
 const pool = require("../config/connection.database");
- const sqlErrorHandler = require("./utils/handle-sql-error");
+const sqlErrorHandler = require("./utils/handle-sql-error");
 
- exports.updateUser = async (id, name, email, password) => {
-     const db = await pool.connect();
-     try {
-         db.query("BEGIN")
-         const query = "UPDATE USUARIOS SET USO_NOME = $1, USO_EMAIL = $2, USO_SENHA = $3 WHERE USO_ID = $4";
-         const result = await db.query(query, [name, email, password, id]);
-         await db.query("COMMIT");
-         return result.rows;
-     } catch (e) {
-         await db.query("ROLLBACK");
-         sqlErrorHandler(e);
-     } finally {
-         db.release();
-     }
- }
+exports.findUserById = async (id) => {
+    const db = await pool.connect();
+    const query = "SELECT * FROM USUARIOS WHERE USO_ID = $1";
+    try {
+        const result = await db.query(query, [id]);
+        return result.rows;
+    } catch (e) {
+        sqlErrorHandler(e);
+    } finally {
+        db.release();
+    }
+}
+
+exports.updateUser = async (id, name, email, password) => {
+    const db = await pool.connect();
+    try {
+        const query = "UPDATE USUARIOS SET USO_NOME = $1, USO_EMAIL = $2, USO_SENHA = $3 WHERE USO_ID = $4";
+        await db.query(query, [name, email, password, id]);
+    } catch (e) {
+        sqlErrorHandler(e);
+    } finally {
+        db.release();
+    }
+}
