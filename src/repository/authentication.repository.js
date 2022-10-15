c4c2018bff320f77d7624d10f48ce36b4a559ec1
fernanda-dionello <fernanda.dionello@gmail.com>
2022-10-15 20:10:06 -0300

    refactor: bring uso id instead email in login route

--- src/repository/authentication.repository.js
+++ src/repository/authentication.repository.js
@@ -4,10 +4,13 @@ const sqlErrorHandler = require("./utils/handle-sql-error");
 
 exports.authUser = async (email, password) => {
     const db = await pool.connect();
-    const query = "SELECT * FROM USUARIOS WHERE USO_EMAIL = $1 AND USO_SENHA = $2";
+    const query = "SELECT uso_id FROM USUARIOS WHERE USO_EMAIL = $1 AND USO_SENHA = $2";
     try {
         const result = await(db.query(query, [email, password]));
-        return result.rowCount;
+        if(result.rowCount > 0){
+            console.log(result.rows[0]);
+            return result.rows[0];
+        }
     } catch(e) {
         sqlErrorHandler(e);
     } finally {
