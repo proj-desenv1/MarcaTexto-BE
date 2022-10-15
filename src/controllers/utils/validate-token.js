0f37080ecc1719792af487d03b6659a49bbc1b58
Thiago Proença <thiago_proenca@sicredi.com.br>
2022-10-15 19:41:37 -0300

    fix: message adjust

--- src/controllers/utils/validate-token.js
+++ src/controllers/utils/validate-token.js
@@ -1,15 +1,14 @@
 const { verify } = require("jsonwebtoken");
 
 exports.tokenValidation = (req, resp, next) => {
-    const message = "Your session has expired"
     const token = req.get("x-session-token");
 
     try {
-        if (!token) return resp.status(401).json({ msg: message });
+        if (!token) return resp.status(401).json({ msg: "You don't have permission to do this request" });
 
         verify(token, process.env.SECRET, (e) => {
             console.log(e)
-            if (e) return resp.status(401).json({ msg: message });
+            if (e) return resp.status(401).json({ msg: "Your session has expired" });
             next();
         });
     } catch(e) {
