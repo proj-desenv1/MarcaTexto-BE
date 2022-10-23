bdd9635b8f7a3270bf7b1cc80281c4bc3121eec1
fernanda-dionello <fernanda.dionello@gmail.com>
2022-10-23 11:15:21 -0300

    test: printing authentication error

--- src/controllers/utils/validate-token.js
+++ src/controllers/utils/validate-token.js
@@ -1,13 +1,20 @@
 const { verify } = require("jsonwebtoken");
+const dotenv = require('dotenv');
+dotenv.config();
 
 exports.tokenValidation = (req, resp, next) => {
     const token = req.get("x-session-token");
 
     try {
-        if (!token) return resp.status(401).json({ msg: "You don't have permission to do this request" });
+        if (!token) {
+            return resp.status(401).json({ msg: "You don't have permission to do this request" });
+        }
 
         verify(token, process.env.SECRET, (e, payload) => {
-            if (e) return resp.status(401).json({ msg: "Your session has expired" });
+            if (e) {
+                console.log('error', e);
+                return resp.status(401).json({ msg: "Your session has expired" });
+            }
             req.userId = payload.uso_id;
             next();
         });
