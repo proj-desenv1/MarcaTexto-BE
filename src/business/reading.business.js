b0b0cb07c4a6bf21544e0714998c8e91ae500ce9
fernanda-dionello <fernanda.dionello@gmail.com>
2022-10-22 21:50:48 -0300

    fix: fix get routes

--- src/business/reading.business.js
+++ src/business/reading.business.js
@@ -27,9 +27,9 @@ exports.startReading = async (userId, bookId, readingStatus, initialPage, curren
     
     const reading = await readingRepository.startReading(userId, bookId, readingStatus, initialPage, currentPage, readingTime);
     
-    const result = await readingRepository.findReadingByBookId(userId, reading.liv_id);
-    
-    return readingsMapper.mapReadings(result, readingStatus)
+    // const result = await readingRepository.findReadingByBookId(userId, reading.liv_id);
+
+    return {readingStatus};
 }
 
 exports.deleteReading = async (userId, bookId) => {
